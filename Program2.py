e0a32c1f56eb3210448085049e83697d03001fd9
OliviafortuneUCD <Test@test.com>
2022-02-03 19:29:50 +0000

    test

--- Program2.py
+++ Program2.py
@@ -1,9 +1,13 @@
 import pandas as pd
 pd1= pd.read_csv('Property.csv')
 #https://www.kaggle.com/claytonb/starter-property-price-register-bfd87ab9-7
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
 
 # selecting rows based on condition
 rslt_df = pd1[pd1['SALE_PRICE'] <= 10000]
+print(rslt_df)
+
 
 
 
--- Program4.py
+++ Program4.py
@@ -1,9 +1,6 @@
 import pandas as pd
-
-
-Dublin= pd.read_csv('Property.csv')
+pd1= pd.read_csv('Property.csv')
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)
-#print(Dublin)
-print(Dublin.head(14))
+
